5338e0c61ba8a7bc9d54f5b2b012be1f405b0950
Adrian Estevez <adrianestevezg@gmail.com>
2025-09-25 14:31:55 +0200

    Fixed powerplants

--- src/energy_production/application/calculate_production_plan.py
+++ src/energy_production/application/calculate_production_plan.py
@@ -13,4 +13,61 @@ class CalculateProductionPlan:
         self.powerplants = powerplants
         
     def execute(self) -> str:
-        pass
+        # ordered_powerplants_by_efficiency = self._ordered_powerplants_by_economic_efficiency()
+        
+        fixed_powerplants = [self._get_fixed_powerplant(powerplant) for powerplant in self.powerplants]
+        fixed_powerplants.sort(key= lambda pwp: pwp.euro_mwh)
+        print([pwp.to_json() for pwp in fixed_powerplants])
+        
+        satiesfied_load = 0
+        solution = []
+        it = 0
+        
+        
+        return "aa"
+    
+    
+    
+    class _FixedPowerPlant:
+        def __init__(self, name, pmin, pmax, euro_mwh):
+            self.name = name
+            self.pmin = pmin
+            self.pmax = pmax
+            self.euro_mwh = euro_mwh
+            
+        def to_json(self):
+            return {
+                "name": self.name,
+                "pmin": self.pmin,
+                "pmax": self.pmax,
+                "euro_mwh" : self.euro_mwh
+            }
+    
+    def _get_fixed_powerplant(self, powerplant: Powerplant) -> _FixedPowerPlant:
+        name = powerplant.name
+        euro_mwh = self._powerplant_economic_efficiency(powerplant)
+        pmin = self.fuels.wind_percentage * powerplant.pmin if powerplant.type == "windturbin" \
+            else powerplant.pmin * powerplant.efficiency
+        pmax = self.fuels.wind_percentage * powerplant.pmax if powerplant.type == "windturbin" \
+            else powerplant.pmax * powerplant.efficiency
+        return self._FixedPowerPlant(name, pmin, pmax, euro_mwh)
+        
+         
+        
+    
+    # def _ordered_powerplants_by_economic_efficiency(self) -> list[Powerplant]:
+    #     return sorted(self.powerplants, key=self._powerplant_economic_efficiency)
+    
+    
+    def _powerplant_economic_efficiency(self, powerplant: Powerplant) -> float:
+        if powerplant.type == "windturbine": 
+            fuel_price = 0
+        elif powerplant.type == "gasfired": 
+            fuel_price = self.fuels.co2_euro_ton
+        else: 
+            fuel_price = self.fuels.kerosine_euro_mwh
+            
+        return fuel_price / powerplant.efficiency
+    
+
+        
